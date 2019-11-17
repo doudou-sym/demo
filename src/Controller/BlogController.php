18ab254e15aa08d068be6d007a8e2d88249752e8
ouerdia <ouerdia.zouba@gmail.com>
2019-11-17 12:24:30 +0100

    form attribu dans twif et afficage avec la fonction form() ou form_widget() pour chaque champ

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -43,74 +43,51 @@ class BlogController extends AbstractController
      */
     public function create(Request $request, ObjectManager $manager)
     {
-        /*dump($request);
-        if($request->request->count()>0){
-            $article=new Article();
-            $article->setTitle($request->request->get('title'))
-                    ->setContent($request->request->get('content'))
-                    ->setImage($request->request->get('image'))
-                    ->setCreatedAt(new \DateTime());
-
-        $manager->persist($article);
-        $manager->flush();
-        //page de redirection apres la creation de l'article
-        return $this->redirectToRoute('blog_show', [
-            'id'=>$article->getId()
-        ]);
-       
-
-        }*/
 
         //article vide
-        $article=new Article();
-
+        $article = new Article();
 
         //createformbulder() pour crer un formulaire a une entite(article)
-        $form=$this->createFormBuilder($article)  //ensuite il faut le configurer avec les champs (ajouter des champs a ce formulaire)
-                  /*->add('title', TextType::class,[
-                       'attr' => [
-                           'placeholder' => "Titre de l'article",
-                           //'class' => 'form-control'
-                       ]
-                   ])
-                   //->add('title', TextType::class)
-                   ->add('content', TextareaType::class)
-                   ->add ('image') 
-                   /*->add('save', SubmitType::class, [
+        $form = $this->createFormBuilder($article)  //ensuite il faut le configurer avec les champs (ajouter des champs a ce formulaire)
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Titre de l'article",
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'placeholder' => "Contenu de l'article",
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('image', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Image de l'article",
+                    'class' => 'form-control'
+                ]
+            ])
+            /*->add('save', SubmitType::class, [
                        'label' => 'enregister'
                    ])*/
-                   /*//ensuite pour voir le resultat finale avec getForm()
-                   ->add('title', TextType::class,[
-                    'attr' => [
-                        'placeholder' => "Titre de l'article",
-                        //'class' => 'form-control'
-                    ]
-                ])*/
-                ->add('title')
-                ->add('content')
-                ->add ('image') 
-                /*->add('save', SubmitType::class, [
-                    'label' => 'enregister'
-                ])*/
-                //ensuite pour voir le resultat finale avec getForm()
-                   ->getForm(); 
+            //ensuite pour voir le resultat finale avec getForm()
+            ->getForm();
 
-                   //Analaliser la requet HTTP passée en parametre, si soumis ou pas
-                   $form->handleRequest($request);
-                  dump($article);
-                  if($form->isSubmitted() && $form->isValid()){
-                      $article->setCreatedAt(new \DateTime());
-                      $manager->persist($article);
-                      $manager->flush();
-                      return $this->redirectToRoute(('blog_show'), ['id'=>$article->getId()]);
-                  }
-                
-
-//ensuite afficher le formulaire, le passer a twig, o, passe pas $form on passe une variable facile a afficher (cretaeView()--> creer un petit objet resultat de ce formulaire)
+        //Analaliser la requet HTTP passée en parametre, si soumis ou pas
+        $form->handleRequest($request);
+        dump($article);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setCreatedAt(new \DateTime());
+            $manager->persist($article);
+            $manager->flush();
+            return $this->redirectToRoute(('blog_show'), ['id' => $article->getId()]);
+        }
 
 
+        //ensuite afficher le formulaire, le passer a twig, o, passe pas $form on passe une variable facile a afficher (cretaeView()--> creer un petit objet resultat de ce formulaire)
         return $this->render("blog/create.html.twig", [
-            'formArticle'=> $form->createView()
+            'formArticle' => $form->createView()
         ]);
     }
 
